638cade6317fa5f9bff7159f1735957f5e30fbf0
JustinRobertKing <kingj4@spu.edu>
2019-05-08 12:16:15 -0700

    initial styling and nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <header className="App-header">
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/shelf">Shelf</Link>
+                  </li>
+                  <li>
+                    <Link to="/cart">Cart</Link>
+                  </li>
+                  <li>
+                    <Link to="/wishlist">Wishlist</Link>
+                  </li>
+                </ul>
+              </nav>
+            </header>
+          </div>
+        </Router>
+      </div>
+    )
+  }
+
 }
 
-export default App;
+export default App
