eb45782311acc4e3ae4431a701011e642bedae76
JustinRobertKing <kingj4@spu.edu>
2019-05-09 14:06:43 -0700

    Updated wishlist actions/reducer

--- src/actions/WishlistActions.js
+++ src/actions/WishlistActions.js
@@ -1,1 +1,13 @@
-WishlistActions.js
+export const addWish = (item) => {
+	return {
+		type: 'ADD_WISH',
+		item
+	}
+}
+
+export const removeWish = (item) => {
+	return {
+		type: 'REMOVE_WISH',
+		item
+	}
+}
